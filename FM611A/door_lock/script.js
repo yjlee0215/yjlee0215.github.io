dfb1b8e5e5c635990ede0c3cff900f949d352250
yjlee0215 <117422079+yjlee0215@users.noreply.github.com>
2022-11-24 22:04:56 +0800

    Add files via upload

--- FM611A/door_lock/script.js
+++ FM611A/door_lock/script.js
@@ -101,9 +101,13 @@ async function recognizeFaces() {
     dis = parseFloat(results[i]["distance"])
     console.log(lab + dis)
 
-    if (lab != "unknown" && dis < 0.4) {
-      $.get(board_url + 'open');
+    if (lab == "yjlee" && dis < 0.5) {
+      $.get(board_url + 'open?name=yjlee');
     }
+    if (lab == "Teddy" && dis < 0.5) {
+      $.get(board_url + 'open?name=Teddy');
+    }
+
 
     const box = resizedDetections[i].detection.box
     const drawBox = new faceapi.draw.DrawBox(box, { label: result })
