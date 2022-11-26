c683dacf3d1d313492a6df6317d65a65b47cc9e5
yjlee0215 <117422079+yjlee0215@users.noreply.github.com>
2022-11-26 09:38:01 +0800

    Update script.js

--- FM611A/door_lock/script.js
+++ FM611A/door_lock/script.js
@@ -104,7 +104,7 @@ async function recognizeFaces() {
     if (lab != "unknown" && dis < 0.6) {
       $.get(board_url + 'open?name=' + lab);
     }
-    console.log(board_url + 'open?name=' + lab)
+    console.log(board_url + 'open?name=yjlee')
 
     const box = resizedDetections[i].detection.box
     const drawBox = new faceapi.draw.DrawBox(box, { label: result })
